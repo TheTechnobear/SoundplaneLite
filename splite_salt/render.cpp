6580e89b5f38286afe723ac0f62541b9c2b16f05
Mark Harris <mharris@technobear.com>
2019-02-19 18:09:12 +0000

    add trig outputs to salt

--- splite_salt/render.cpp
+++ splite_salt/render.cpp
@@ -25,7 +25,6 @@ SPTouch rPanel_ 	{0,false,0.0f,0.0f,0.0f,0.0f};
 SPTouch rSlider_ 	{0,false,0.0f,0.0f,0.0f,0.0f};
 
 #ifdef SALT
-	#pragma message("with added SALT")
 	#define OUT_VOLT_RANGE 10.0f
 	#define OUT_OFFSET 0.5f
 #else 
@@ -46,39 +45,43 @@ public:
     
    void touchOn(unsigned  tId, float x, float y, float z) override {
         // std::cout << " touchOn:" << tId << " x:" << x  << " y:" << y << " z:" << z << std::endl;
-        updateTouch(tId,x,y,z);
+        updateTouch(tId,true,x,y,z);
     }
 
     void touchContinue(unsigned tId, float x, float y, float z) override {
         // std::cout << " touchContinue:" << tId << " x:" << x  << " y:" << y << " z:" << z << std::endl;
-        updateTouch(tId,x,y,z);
+        updateTouch(tId,true, x,y,z);
 
     }
 
     void touchOff(unsigned tId, float x, float y, float z) override {
         // std::cout << " touchOff:" << tId << " x:" << x  << " y:" << y << " z:" << z << std::endl;
-        updateTouch(tId,x,y,0.0f);
+        updateTouch(tId,false, x,y,0.0f);
     }
 private:
 	static constexpr float semiMult = (1.0f / (OUT_VOLT_RANGE * 12.0f)); // 1.0 = 10v = 10 octaves 
-	void updateTouch(unsigned  tId, float x, float y, float z) {
+	void updateTouch(unsigned  tId, bool active,float x, float y, float z) {
         if(x < 2.0f) {
         	lSlider_.id_ = tId;
+        	lSlider_.active_ = active;
         	lSlider_.y_ = y / 5.0f;
         } else if(x < 12.0f) {
         	lPanel_.id_ = tId;
+        	lPanel_.active_ = active;
         	lPanel_.x_ = (x - 2.0f)  / 10.0f ;
         	lPanel_.y_ = y / 5.0f;
-        	lPanel_.z_ = (z / 2.0) + OUT_OFFSET;
+        	lPanel_.z_ = ((z / 2.0) *2.0f)  + OUT_OFFSET;
         	lPanel_.pitch_ =  ((x - 2.0f)  * semiMult) + OUT_OFFSET;
         } else if(x < 28.0f) {
         	rPanel_.id_ = tId;
+        	rPanel_.active_ = active;
         	rPanel_.x_ = (x - 12.0f)  / 16.0f;
         	rPanel_.y_ = y / 5.0f;
-        	rPanel_.z_ = (z / 2.0) + OUT_OFFSET;
+        	rPanel_.z_ = ((z / 2.0) * 2.0f) + OUT_OFFSET;
         	rPanel_.pitch_ =  ((x - 12.0f)  * semiMult) + OUT_OFFSET;
         } else {
         	rSlider_.id_ = tId;
+        	rSlider_.active_ = active;
         	rSlider_.y_ = y / 5.0f;
         }
 	}
@@ -94,14 +97,17 @@ void process_salt(void*) {
 
 bool setup(BelaContext *context, void *userData)
 {
+	pinMode(context, 0, 0, OUTPUT); 
+	pinMode(context, 0, 5, OUTPUT); 
+	pinMode(context, 0, 12, OUTPUT); 
+	pinMode(context, 0, 13, OUTPUT); 
+	
 	gpDevice = new SPLiteDevice();
     gpDevice->addCallback(gCallback);
     gpDevice->maxTouches(4);
     gpDevice->start();
 	
     // Initialise auxiliary tasks
-
-	// if((gSPLiteProcessTask = Bela_createAuxiliaryTask( process_salt, 0, "SPLiteProcessTask", gpDevice)) == 0)
 	if((gSPLiteProcessTask = Bela_createAuxiliaryTask( process_salt, BELA_AUDIO_PRIORITY - 5, "SPLiteProcessTask", gpDevice)) == 0)
 		return false;
 
@@ -128,17 +134,24 @@ void render(BelaContext *context, void *userData)
 	    // distribute touches to cv
 		Bela_scheduleAuxiliaryTask(gSPLiteProcessTask);
 	// }
+	
+	for(unsigned int n = 0; n < context->digitalFrames; n++) {
+		if(0  < context->digitalChannels) { digitalWriteOnce(context, n, 0,  lSlider_.active_);}	// trig1
+		if(5  < context->digitalChannels) { digitalWriteOnce(context, n, 5,  lPanel_.active_);}	// trig2
+		if(12  < context->digitalChannels) { digitalWriteOnce(context, n, 12,  rPanel_.active_);}	// trig3
+		if(13  < context->digitalChannels) { digitalWriteOnce(context, n, 13,  rSlider_.active_);}	// trig4
+	}
 
 	for(unsigned int n = 0; n < context->analogFrames; n++) {
 		unsigned ch = 0;
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, lSlider_.y_); ch++; };// CV 1
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, lPanel_.pitch_ ); ch++; };// CV 2
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, lPanel_.y_ ); ch++; };// CV 3
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, lPanel_.z_ ); ch++; };// CV 4
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, rPanel_.pitch_ ); ch++; };// CV 5
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, rPanel_.y_ ); ch++; };// CV 6
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, rPanel_.z_ ); ch++; };// CV 7
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, rSlider_.y_); ch++; };// CV 8
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, lSlider_.y_); ch++; }		// CV 1
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, lPanel_.pitch_ ); ch++; }	// CV 2
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, lPanel_.y_ ); ch++; } 	// CV 3
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, lPanel_.z_ ); ch++; } 	// CV 4
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, rPanel_.pitch_ ); ch++; } // CV 5
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, rPanel_.y_ ); ch++; }		// CV 6
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, rPanel_.z_ ); ch++; } 	// CV 7
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, rSlider_.y_); ch++; } 	// CV 8
 	}
 
 }
